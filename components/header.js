6bbec2ee9fc597587738d142d3aa29cd2ceb0b08
Mukul Dogra <mukulwritescode@gmail.com>
2025-02-23 23:48:35 +0530

    Added Animated Responsive Header

--- components/header.js
+++ components/header.js
@@ -11,24 +11,59 @@ export function toggleUserActions(loggedIn) {
   }
 }
 
+export function navToggle() {
+  let icon1 = document.getElementById("a");
+  let icon2 = document.getElementById("b");
+  let icon3 = document.getElementById("c");
+  let nav = document.getElementById('nav');
+  let black = document.getElementById("black");
+
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('show');
+  black.classList.toggle('slide');
+};
+
 export function renderHeader() {
   const headerElement = document.querySelector('.js-header');
   const headerHTML = `
-     <nav class="nav-container">
-        <div class="logo-container">
-          <img src="images/agro-kiran-dark.png" alt="">
+      <nav class="flex justify-between items-center">
+        <!-- Logo -->
+        <img class="w-[60px] duration-170 hover:scale-105" src="images/agro-kiran-dark.png" alt="-brand-logo" />
+        <!-- Nav Links -->
+        <ul class="hidden md:flex gap-8 text-white ml-20 flex-1 justify-center">
+          <li>
+            <a class="nav-links" href="#">Home</a>
+          </li>
+          <li><a class="nav-links" href="#">Shop</a></li>
+          <li>
+            <a class="nav-links" href="#">About</a>
+          </li>
+          <li>
+            <a class="nav-links" href="#">Contact</a>
+          </li>
+        </ul>
+        <!-- Cart Icon -->
+        <div class="mr-4 hidden md:flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-secondary opacity-75 -top-2.5 left-2.5"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-secondary -top-2.5 left-2.5"></span>
+          </span>
+          <i class="fa-solid fa-cart-shopping text-white text-[20px]"></i>
         </div>
 
-        <ul class="nav-links">
-          <li><a href="#" class="active">Home</a></li>
-          <li><a href="#">Shop</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-
-        <div class="user-actions">
-          <a href="" class="login user">Login</a>
-          <a href="" class="sign-up user">Sign Up</a>
+        <!-- Login Signup -->
+        <div class="hidden md:flex gap-2 items-center justify-center">
+          <!-- Buttons -->
+          <div class="js-ls-btns flex gap-2">
+            <a href="" class="user-actions bg-white text-primary hover:bg-transparent">Login</a>
+            <a href="" class="user-actions text-white">Sign Up</a>
+          </div>
+          <!-- User Menu -->
+          <a class="js-user-menu relative hidden justify-center items-center border-2 border-white rounded-full size-[28px]" href="">
+            <i class="fa-solid fa-user text-white text-[12px]"></i>
+          </a>
         </div>
       </nav>
     `;
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-import { loggedIn, renderHeader, toggleUserActions } from "./components/header.js";
+import { renderHeader, navToggle, toggleUserActions } from "./components/header.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    // renderHeader();
-    toggleUserActions(loggedIn);
+    renderHeader();
+    toggleUserActions();
+    let icon = document.getElementById("icon");
+    let black = document.getElementById("black");
+    icon.addEventListener('click', navToggle);
+    black.addEventListener('click', navToggle);
 })
